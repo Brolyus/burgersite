450764d1af14b4fa15aac0d2502930fc2eeb37a6
Anthony De Sousa <anthonydesousa@macbook-pro-de-anthony.home>
2020-04-22 00:12:01 +0200

    ajout des ingredients possible, step 133 done

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,49 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENT_PRICES = {
+    salad: 1,
+    cheese: 1.5,
+    meat: 2,
+    bacon: 0.5
+}
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        },
+        totalPrice: 10
+    }
+    
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCounted = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCounted
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredientHandler = (type) => {
+        
+    }
+
     render() {
         return (
             <Aux>
-                <Burger />
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}/>
             </Aux>
         );
     }
